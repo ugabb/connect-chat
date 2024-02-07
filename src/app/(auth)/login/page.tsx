8a3968f9670fe681d73a7676d94f765cceaacdec
ugabb <gsb.alternativo@gmail.com>
2024-02-07 07:22:14 -0300

    update: login page to better feedbacks

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { signIn, signOut } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import { FaGoogle } from "react-icons/fa";
 import toast from 'react-hot-toast';
@@ -13,23 +13,40 @@ import axios from 'axios';
 import { userCredentialsValidation, userCredentialsValidationLogin } from '@/lib/validations/user-credentials';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
+import { PiCircleNotch } from 'react-icons/pi';
+import Link from 'next/link';
 
 type FormData = z.infer<typeof userCredentialsValidationLogin>
 
-const Register = () => {
+const Login = () => {
   const [loading, setLoading] = useState<boolean>(false)
 
   const router = useRouter();
+  const session = useSession();
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/dashboard')
+    }
+  }, [session?.status, router]);
+
 
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-    resolver: zodResolver(userCredentialsValidation)
+    resolver: zodResolver(userCredentialsValidationLogin)
   })
 
-  const loginWithGoogle = async () => {
+  const loginWithGoogle = () => {
     try {
       setLoading(true)
-      await signIn('google')
+      signIn('google', { redirect: false }).then((callback) => {
+        if (callback?.ok) {
+          toast.success(`Bem vindo!`);
+          router.push("/dashboard")
+        }
+        if (callback?.error) {
+          toast.error(`Erro ao conect com o Google!`);
+        }
+      }).finally(() => setLoading(false))
     } catch (error) {
       toast.error("Alguma coisa deu errado com o seu login ☹")
     } finally {
@@ -45,36 +62,43 @@ const Register = () => {
         password: data.password,
         redirect: false,
       });
-      console.log(successSignIn)
-      if (successSignIn) {
-        // toast.success(`Bem vindo ${}`);
+      if (successSignIn?.ok) {
+        toast.success(`Bem vindo!`);
+        router.push("/dashboard")
       }
     } catch (error) {
       toast.error(`Erro ao registrar usuário: ${error}`);
     }
   }
 
+
   return (
-    <div className='flex flex-col justify-center items-center mx-auto p-5 max-w-sm'>
+    <div className='flex flex-col justify-center items-center mx-auto p-5 max-w-sm space-y-5'>
       <h1 className='text-xl font-bold text-main'>Sign In to Chat!</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5'>
+      <form onSubmit={handleSubmit(onSubmit)} id="loginForm" className='flex flex-col gap-5'>
         <label className='text-sm'>
           Email
           <Input className='w-full' {...register("email", { required: true })} type='email' placeholder='gabriel@mail.com' />
         </label>
         <label className='text-sm'>
           Password
-          <Input className='w-full' {...register("password", { required: true })} type='password' placeholder='Gabriel' />
+          <Input className='w-full' {...register("password", { required: true })} type='password' placeholder='password' />
         </label>
         <div className="flex items-center justify-center gap-3">
-          <Button className='flex gap-1 bg-transparent border border-main text-main hover:text-white hover:bg-main' type='submit'>Sign In</Button>
-          <Button className='flex gap-1 bg-transparent border border-main text-main hover:text-white hover:bg-main' onClick={loginWithGoogle}><FaGoogle /></Button>
-          {loading && <p>Loading</p>}
+          <Button className='flex gap-1 bg-transparent border border-main text-main hover:text-white hover:bg-main' type='submit' form='loginForm'>Sign In</Button>
+          <Button className='flex gap-1 bg-transparent border border-main text-main hover:text-white hover:bg-main' type='button' onClick={loginWithGoogle}>
+            {loading ? <PiCircleNotch className='animate-spin text-main' /> : <FaGoogle />}
+          </Button>
         </div>
       </form>
+
+      <p>Não tem uma conta? <Link href={"/register"} className='text-main font-semibold hover:underline'>Crie uma conta!</Link> </p>
     </div>
   )
+
+
+
 }
 
-export default Register
+export default Login
