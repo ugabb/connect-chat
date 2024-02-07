1d2cb40ab899fa2d8f7faa53ddecd2570b095912
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-02-07 11:55:12 -0300

    fix: data prop of user in server component

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'lh3.googleusercontent.com',
-              pathname: '**',
-            },
-          ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+  /** Configuração Experimental com SWC:
+ * 
+ * Utiliza a funcionalidade experimental do Next.js para incorporar plugins SWC durante a compilação.
+ * Aqui, adicionamos o plugin 'next-superjson-plugin' para otimizar operações JSON.
+ * O objeto vazio '{}' indica a utilização da configuração padrão para o 'next-superjson-plugin'.
+ */
+  experimental: {
+    swcPlugins: [
+      ["next-superjson-plugin", {}]
+    ]
+  }
 };
 
 export default nextConfig;
