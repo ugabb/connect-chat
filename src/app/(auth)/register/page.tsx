82aee45f9754d396bb7e2ab4ce3b122c2c3fdab5
ugabb <gsb.alternativo@gmail.com>
2024-02-07 07:22:48 -0300

    update: creates in register page errors feedback

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -14,6 +14,7 @@ import { userCredentialsValidation } from '@/lib/validations/user-credentials';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 type FormData = z.infer<typeof userCredentialsValidation>
 
@@ -22,7 +23,7 @@ const Register = () => {
 
     const router = useRouter();
 
-    const { register, handleSubmit, formState: { errors }} = useForm<FormData>({
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
         resolver: zodResolver(userCredentialsValidation)
     })
 
@@ -37,45 +38,57 @@ const Register = () => {
         }
     }
 
-    const onSubmit: SubmitHandler<FormData> = async (data: any) => {
-        try {
-            const response = await axios.post('/api/register', data)
-            const user: FormData = response.data;
-            if (response.statusText === "OK") {
-                await signIn('credentials', {
-                    name: user.name,
-                    email: user.email,
-                    password: user.password,
-                    redirect: false,
-                });
-                toast.success(`Usuário registrado com sucesso`);
+    const onSubmit: SubmitHandler<FormData> = async (data: FormData) => {
+        const credentialsValidated = userCredentialsValidation.parse(data)
+        if (!credentialsValidated.name || !credentialsValidated.email || !credentialsValidated.password) {
+            return toast.error("Erro com seus dados. Verifique e tente novamente!")
+        } else {
+
+            try {
+                const response = await axios.post('/api/register', data)
+                const user: FormData = response.data;
+                if (response.statusText === "OK") {
+                    await signIn('credentials', {
+                        name: user.name,
+                        email: user.email,
+                        password: user.password,
+                        redirect: false,
+                    });
+                    toast.success(`Usuário registrado com sucesso`);
+                    router.push("/dashboard")
+                }
+            } catch (error) {
+                toast.error(`Erro ao registrar usuário: ${error.response.data.error}`);
             }
-        } catch (error) {
-            toast.error(`Erro ao registrar usuário: ${error.response.data.error}`);
         }
     }
 
     return (
-        <div className='flex flex-col justify-center items-center mx-auto p-5 max-w-sm'>
+        <div className='flex flex-col justify-center items-center mx-auto p-5 max-w-sm space-y-5'>
             <h1 className='text-xl font-bold text-main'>Sign In to Chat!</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5'>
                 <label className='text-sm'>
                     Nome
                     <Input className='w-full' {...register("name", { required: true })} type='text' placeholder='Gabriel' />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
                 </label>
                 <label className='text-sm'>
                     Email
                     <Input className='w-full' {...register("email", { required: true })} type='email' placeholder='gabriel@mail.com' />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                 </label>
                 <label className='text-sm'>
                     Password
-                    <Input className='w-full' {...register("password", { required: true })} type='password' placeholder='Gabriel' />
+                    <Input className='w-full' {...register("password", { required: true, min: 6 })} type='password' placeholder='Gabriel' />
+                    {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                 </label>
                 <div className="flex items-center justify-center gap-3">
                     <Button className='flex gap-1 bg-transparent border border-main text-main hover:text-white hover:bg-main' type='submit'>Sign Up</Button>
                 </div>
             </form>
+
+            <p>Já tem uma conta? <Link href={"/login"} className='text-main font-semibold hover:underline'>Entra na sua conta!</Link> </p>
         </div>
     )
 }
