b7dbb90c405b927c2de8997c008addc974a2ff9f
ugabb <gsb.alternativo@gmail.com>
2024-02-07 07:21:11 -0300

    feat: create a context to provide for application. - Auth Context - Toaster Context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "@/components/Providers";
+import ToasterContext from "./context/ToasterProvider";
+import AuthContext from "./context/AuthConetxt";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+        <ToasterContext />
+
+        <AuthContext>
           {children}
-        </Providers>
+        </AuthContext>
       </body>
     </html>
   );
