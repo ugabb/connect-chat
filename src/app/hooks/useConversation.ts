c218a4c8af19800b62b7c681c6fec63a0a7b1163
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-02-08 09:42:25 -0300

    fix: delete duplicated files

--- src/app/hooks/useConversation.ts
+++ src/app/hooks/useConversation.ts
@@ -1,19 +1,26 @@
 import { useParams } from "next/navigation";
 import { useMemo } from "react";
 
+
 const useConversation = () => {
+  // Obtendo os parâmetros da URL usando o hook useParams do Next.js
   const params = useParams();
 
+  // Utilizando useMemo para memorizar o valor de conversationId
   const conversationId = useMemo(() => {
+    // Verificando se não há um parâmetro conversationId na URL
     if (!params?.conversationId) {
       return "";
     }
 
+    // Retornando o valor do parâmetro conversationId como uma string
     return params.conversationId as string;
   }, [params?.conversationId]);
 
+  // Utilizando useMemo para determinar se a conversa está aberta com base no conversationId
   const isOpen = useMemo(() => !!conversationId, [conversationId]);
 
+  // Retornando um objeto com informações sobre a conversa, utilizando useMemo para memoizar o resultado
   return useMemo(
     () => ({
       isOpen,
@@ -22,4 +29,5 @@ const useConversation = () => {
     [isOpen, conversationId]
   );
 };
+
 export default useConversation;
