47062dab3374e8b4ad2501380c98d3b70bcd2bdf
ugabb <gsb.alternativo@gmail.com>
2024-02-14 10:04:51 -0300

    fix: min width set to avoid distortion o avatar group

--- src/components/AvatarGroup.tsx
+++ src/components/AvatarGroup.tsx
@@ -1,43 +1,46 @@
-
-
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 
 interface AvatarGroupProp {
-    users?: User[]
+  users?: User[];
 }
 
 const AvatarGroup = ({ users = [] }: AvatarGroupProp) => {
-    const slicedUsers = users.slice(0, 3);
+  const slicedUsers = users.slice(0, 3);
 
-    const positionMap = {
-        0: 'top-0 left-[12px]',
-        1: 'bottom-0 left-0',
-        2: 'bottom-0 right-0'
-    }
-    return (
-        <div className="relative h-11 w-11">
-            {slicedUsers.map((user, index) => (
-                <div
-                    key={user.id}
-                    className={`
-                absolute
-                inline-block 
-                rounded-full 
-                overflow-hidden
-                h-[21px]
-                w-[21px]
-                ${positionMap[index as keyof typeof positionMap]}
-              `}>
-                    <Image
-                        fill
-                        src={user?.image || ''}
-                        alt="Avatar"
-                    />
-                </div>
-            ))}
+  const positionMap = {
+    0: 'top-0 left-[12px]',
+    1: 'bottom-0 left-0',
+    2: 'bottom-0 right-0',
+  };
+
+  return (
+    <div className="relative min-h-11 min-w-11">
+      {slicedUsers.map((user, index) => (
+        <div
+          key={user.id}
+          className={`
+            absolute
+            inline-block 
+            rounded-full 
+            overflow-hidden
+            h-[21px]
+            w-[21px]
+            ${positionMap[index as keyof typeof positionMap]}
+          `}
+        >
+          <Image
+          
+            src={user?.image || ''}
+            alt="Avatar"
+            className="rounded-full"
+            width={21}
+            height={21}
+          />
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-export default AvatarGroup
+export default AvatarGroup;
